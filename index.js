f664e6a6449a5ab9cc26cbe7b6b2c6547537483f
Alireza Ftekhari <alireza1377eftekhari@gmail.com>
2020-10-22 12:20:02 +0330

    user finised

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const app = require('express')();
+app.use(require('express').static('public'));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-
+var clients = [];
 io.on('connection', (socket) => {
     console.log(socket.id + ' connected');
-
+    clients.push(socket.id);
     socket.on('disconnect', () => {
         console.log(socket.id + ' disconnected');
+        let index = clients.indexOf(socket.id);
+        if(index != -1) {
+            clients.splice(index, 1);
+        }
+
     });
+
+    socket.on('len', () => {
+        console.log(`-----------------${clients.length} -------------------`);
+    })
 });
 
 
